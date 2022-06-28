21277ce493eda72e057ba8ee90ff9b024050be4e
rodri_rod <47514098+rodrigotorres24@users.noreply.github.com>
2022-06-27 23:14:47 -0700

    Add files via upload

---
+++ CameraActivity.java
@@ -0,0 +1,15 @@
+package com.example.espejosimple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
